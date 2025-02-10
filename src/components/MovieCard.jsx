3ee6d2ec396e429dff7b5f0b6c95ad09af2bac99
Jessica Vaz <168509445+Jessicavazm@users.noreply.github.com>
2025-02-10 11:38:54 +1100

    feat: Dynamically changed the 'add to watchlist' button after it's clicked.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { useWatchlist } from '../context/WatchlistContext';
 import { Link } from 'react-router-dom';
@@ -6,7 +7,9 @@ import { Link } from 'react-router-dom';
 export default function MovieCard({ movie }) {
 
     // Import useWatchlist Hook
-    const { addToWatchlist } = useWatchlist();
+    const { watchlist, addToWatchlist } = useWatchlist();
+
+    const isInWatchlist = watchlist.some((item) => item.imdbID === movie.imdbID);
 
     return (
         <div className="col-md-4 movie-card" style={{ maxWidth: '18 rem' }}>
@@ -15,7 +18,14 @@ export default function MovieCard({ movie }) {
             <img src={movie.Poster}
                 alt={`${movie.Title} Poster`}
                 className='card-img-top' />
-            <button onClick={() => addToWatchlist(movie)}>Add to Watchlist</button>
+            <button
+                className={`btn ${isInWatchlist ? `btn-success` : `btn-primary`}`}
+                // if its not in the watchlist, then add it to list
+                onClick={() => !isInWatchlist && addToWatchlist(movie)}
+                disabled={isInWatchlist}
+            >
+                {isInWatchlist ? `Added to Watchlist` : `Add to Watchlist`}
+            </button>
             <Link to={`/details/${movie.imdbID}`} className='btn btn-primary'>View Details</Link>
         </div>
     )
